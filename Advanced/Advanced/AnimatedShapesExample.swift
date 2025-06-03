674941e206d19729c28180e0279cf73d1cb41528
Mohammed Anish B <anish@incubyte.co>
2025-06-03 17:18:02 +0530

    Animated Shapes Example

---
+++ Advanced/Advanced/AnimatedShapesExample.swift
@@ -0,0 +1,85 @@
+//
+//  AnimatedShapesExample.swift
+//  Advanced
+//
+//  Created by Mohammed Anish B on 03/06/25.
+//
+
+import SwiftUI
+
+struct AnimateableShapesExample: View {
+    
+    @State private var animate: Bool = false
+    
+    var body: some View {
+        ZStack {
+//            RoundedRectangle(cornerRadius: animate ? 60 : 0)
+//            RectangleWithSingleCornerAnimation(cornerRadius: animate ? 60 : 0)
+            Pacman(offsetAmount: animate ? 20 : 0)
+                .frame(width: 250, height: 250)
+        }
+        .onAppear {
+            withAnimation(Animation.easeInOut.repeatForever()) {
+                animate.toggle()
+            }
+        }
+    }
+}
+
+struct AnimateableShapesExample_Previews: PreviewProvider {
+    static var previews: some View {
+        AnimateableShapesExample()
+    }
+}
+
+struct RectangleWithSingleCornerAnimation: Shape {
+    
+    var cornerRadius: CGFloat
+    
+    var animatableData: CGFloat {
+        get { cornerRadius }
+        set { cornerRadius = newValue }
+    }
+    
+    func path(in rect: CGRect) -> Path {
+        Path { path in
+            path.move(to: .zero)
+            path.addLine(to: CGPoint(x: rect.maxX, y: rect.minY))
+            path.addLine(to: CGPoint(x: rect.maxX, y: rect.maxY - cornerRadius))
+            
+            path.addArc(
+                center: CGPoint(x: rect.maxX - cornerRadius, y: rect.maxY - cornerRadius),
+                radius: cornerRadius,
+                startAngle: Angle(degrees: 0),
+                endAngle: Angle(degrees: 360),
+                clockwise: false)
+            
+            path.addLine(to: CGPoint(x: rect.maxX - cornerRadius, y: rect.maxY))
+            path.addLine(to: CGPoint(x: rect.minX, y: rect.maxY))
+        }
+    }
+    
+}
+
+struct Pacman: Shape {
+    
+    var offsetAmount: Double
+    
+    var animatableData: Double {
+        get { offsetAmount }
+        set { offsetAmount = newValue }
+    }
+    
+    func path(in rect: CGRect) -> Path {
+        Path { path in
+            path.move(to: CGPoint(x: rect.midX, y: rect.minY))
+            path.addArc(
+                center: CGPoint(x: rect.midX, y: rect.minY),
+                radius: rect.height / 2,
+                startAngle: Angle(degrees: offsetAmount),
+                endAngle: Angle(degrees: 360 - offsetAmount),
+                clockwise: false)
+        }
+    }
+    
+}
