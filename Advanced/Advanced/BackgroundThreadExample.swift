16fafeb23bfb99b5978a90b857e8fb65063737d8
Mohammed Anish B <anish@incubyte.co>
2025-06-09 17:21:02 +0530

    Background Thread Example

---
+++ Advanced/Advanced/BackgroundThreadExample.swift
@@ -0,0 +1,73 @@
+//
+//  BackgroundThreadExample.swift
+//  Advanced
+//
+//  Created by Mohammed Anish B on 09/06/25.
+//
+
+import SwiftUI
+
+class BackgroundThreadViewModel: ObservableObject {
+    
+    @Published var dataArray: [String] = []
+    
+    
+    func fetchData() {
+        
+        DispatchQueue.global(qos: .background).async {
+            let newData = self.downloadData()
+            
+            print("CHECK 1: \(Thread.isMainThread)")
+            print("CHECK 1: \(Thread.current)")
+            
+            DispatchQueue.main.async {
+                self.dataArray = newData
+                print("CHECK 2: \(Thread.isMainThread)")
+                print("CHECK 2: \(Thread.current)")
+            }
+            
+        }
+        
+    }
+    
+    private func downloadData() -> [String] {
+        var data: [String] = []
+        for x in 0..<100 {
+            data.append("\(x)")
+            print(data)
+        }
+        return data
+    }
+    
+    
+}
+
+struct BackgroundThreadExample: View {
+    
+    @StateObject var vm = BackgroundThreadViewModel()
+    
+    var body: some View {
+        ScrollView {
+            LazyVStack(spacing: 10) {
+                Text("LOAD DATA")
+                    .font(.largeTitle)
+                    .fontWeight(.semibold)
+                    .onTapGesture {
+                        vm.fetchData()
+                    }
+                
+                ForEach(vm.dataArray, id: \.self) { item in
+                    Text(item)
+                        .font(.headline)
+                        .foregroundColor(.red)
+                }
+            }
+        }
+    }
+}
+
+struct BackgroundThreadExample_Previews: PreviewProvider {
+    static var previews: some View {
+        BackgroundThreadExample()
+    }
+}
